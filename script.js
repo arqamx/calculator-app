d997a6b70fd687c66cef1d288c1c85fa9659cb7e
techStarZx <stechstar.me@gmail.com>
2023-08-03 09:31:16 +0500

    clear output on-click Clear button

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let btnContainer = document.getElementById('btn-container');
+let output = document.getElementById('result');
+
 btnContainer.addEventListener('click', (event) => {
     if(!event.target.matches('button')) return;
 
+    let btnType = event.target.className; 
 
-})
+    if (btnType == 'clear') {
+        output.innerText = '0';
+    }
+});
